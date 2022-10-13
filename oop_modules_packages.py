d3f1de450f8106408134897d7f2268eafc29d824
Ayoub-Igo10 <ayoub.igo10@outlook.com>
2022-10-13 12:33:11 +0100

    Updates

--- oop_modules_packages.py
+++ oop_modules_packages.py
@@ -1,6 +1,44 @@
 # lucky draw
 # key word called import
 # import random
+# from random import *
+# import math
 
-print(random.random())
-# each time it's run it generates a random number
+# print(random)
+
+#print(random.random())
+# print(random())
+
+# each time it's run it generates a random number - 0-1
+
+# calculate DOB or year of birth
+
+# from random import *
+import math
+
+
+number = 23.66
+# any numbers - round this figure up
+# number 1.50 above to round up 2
+# numbers 1.49 or less to round down 1
+print(math.ceil(number)) # ceil will help you round up
+print(math.floor(number)) # floor will help you round down
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
